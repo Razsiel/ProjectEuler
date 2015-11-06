c23d22fd11eec12e40151255edd4b102b1477719
geoffrey <geoffreyarkenbout@gmail.com>
2015-11-06 23:21:54 +0100

    Added implementation for Problem1 (Working) Added Problem3 Summary to xml file

--- ConsoleApplication1/Problems/1-10/Problem1.cs
+++ ConsoleApplication1/Problems/1-10/Problem1.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ProjectEulerMain
 {
     public class Problem1 : IProblem
     {
+        const int MAXRANGE = 1000;
         public void FixProblem()
         {
-            Console.WriteLine("This problem has not been fully implemented yet...");
+            List<int> numbers = new List<int>();
+            for (int i = 1; i < MAXRANGE; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    numbers.Add(i);
+            }
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Natural numbers that are a multiple of 3 or 5:");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine( string.Join(", ", numbers.Select(n => n)));
+            Console.WriteLine();
+            
+            int sum = numbers.Sum();
+            Console.WriteLine("Sum of all numbers in collection: {0}", sum);
         }
     }
 }
